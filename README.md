0e276b40801c3579ddf156bc31b872996198ee3d
Halle-Astra <halle.wang@qq.com>
2020-12-14 11:38:23 +0800

    add pre..mv.py

--- README.md
+++ README.md
@@ -10,6 +10,18 @@ This code is copied and adapted for my Windows enviroment.
 
 The usage of this code is the same as https://github.com/Rudrabha/Lip2Wav .
 
+## preprocess\_win\_mv.py
+The pathes replacements in preprocess.py have problem on Windows. 
+But the cost of that code is so long that I write this code to move files .(10.5 hours of dl speaker's)
+
+You need run as : 
+`python preprocess_win_mv.py <name>`
+
+Using Example :
+`python preprocess_win_mv.py dl`
+
+<font color = 'red'>This code is quicker a lot than `preprocess\_win\_cp.py`.</font> 
+
 ## preprocess\_win\_cp.py
 The pathes replacements in `preprocess.py` have problem on Windows and I repaired this bug.
 But the cost of that code is so long that I write this code to move files .(about 10.5 hours of dl speaker's)
@@ -22,3 +34,4 @@ Using Example :
 
 If you want to remove directory of speaker after files movement, you can add the argument as `--remove_dir`:
 `python preprocess_win_cp.py dl --remove_dir`
+
---
+++ preprocess_win_mv.py
@@ -0,0 +1,44 @@
+#preprocessing on windows by copiny
+#由于作者的代码中路径切割与替换是针对linux的，而在我发现之前已经用了10.5小时运行preprocess，而其路径在win下失误，故而写此代码进行手动移动文件。
+'''
+The pathes replacements in preprocess.py have problem on Windows. 
+But the cost of that code is so long that I write this code to move files .(10.5 hours of dl speaker's)
+
+You need run as : 
+python preprocess_win_mv.py <name>
+
+Using Example :
+python preprocess_win_mv.py dl
+'''
+import os 
+import shutil 
+import sys 
+from tqdm import tqdm 
+from multiprocessing import Pool 
+
+def move_dir(dir_,cut_dir,res_root):
+	dir_fe = os.path.split(dir_)[-1] # dir final element
+	cut_path = os.path.join(dir_,cut_dir)
+	des_path = os.path.join(res_root,dir_fe,cut_dir)
+	if not os.path.exists(des_path):
+		os.makedirs(des_path)
+	shutil.move(cut_path,des_path)
+
+if __name__ == '__main__':
+	root = 'Dataset'
+	speaker = sys.argv[1]
+	root = os.path.join(root,speaker)
+	intervals_root = os.path.join(root,'intervals')
+	res_root = os.path.join(root,'preprocessed')
+	dirs_ = os.listdir(intervals_root)
+	dirs_ = [os.path.join(intervals_root,i) for i in dirs_]
+	pbar = tqdm(total=len(dirs_)) # each cut directory copying
+	for dir_ in dirs_:
+		dir_temp = os.listdir(dir_)
+		dir_temp = [i for i in dir_temp if '.mp4' not in i]
+		pool = Pool(8)
+		for cut_dir in dir_temp:
+			pool.apply_async(move_dir,args = (dir_,cut_dir,res_root))
+		pool.close()
+		pool.join()
+		pbar.update(1)
